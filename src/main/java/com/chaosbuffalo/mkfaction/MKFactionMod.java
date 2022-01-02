85c31734dad0b33f04aff94890af85eed5b92464
Kovak <kovac1066@gmail.com>
2022-01-01 16:10:47 -0800

    improve faction serialization

--- src/main/java/com/chaosbuffalo/mkfaction/MKFactionMod.java
+++ src/main/java/com/chaosbuffalo/mkfaction/MKFactionMod.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
